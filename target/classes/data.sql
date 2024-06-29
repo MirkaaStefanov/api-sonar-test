f16c561dba9592d8854342d28634a76fccf43816
MirkaaStefanov <mirkanstefanov2007@gmail.com>
2024-06-30 02:28:04 +0300

    initial commit

---
+++ target/classes/data.sql
@@ -0,0 +1,3 @@
+INSERT INTO libraries(id ,name ,location ,users_count) values (DEFAULT, 'Billy Jean', 'Neshto', 3);
+INSERT INTO libraries(id ,name ,location ,users_count) values (DEFAULT, 'Trg-biblioteka', 'Targovishte', 200);
+INSERT INTO libraries(id ,name ,location ,users_count) values (DEFAULT, 'Varnenska', 'Varna', 200);
